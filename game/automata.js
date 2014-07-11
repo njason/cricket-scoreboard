614ff3733180a5d3569fea0549115d2fe3f7e8de
Jason Biegel <jasonb@mediaradar.com>
2014-07-11 14:37:01 -0400

    un-minified code

--- game/automata.js
+++ game/automata.js
@@ -1,1 +1,90 @@
-var orderPreference=["20","19","18","17","16","15","B"];function runPointLogic(a,f){var c=-1;var e=-1;var b=-1;for(var d=0;7>d;++d){if(3<=a[orderPreference[d]]&&3>f[orderPreference[d]]){b=d;break}else{if(3>a[orderPreference[d]]&&3>f[orderPreference[d]]&&-1==c){c=d}else{if(3>a[orderPreference[d]]&&3<=f[orderPreference[d]]&&-1==e){e=d}}}}if(-1!=b){return orderPreference[b]}else{if(-1!=c){return orderPreference[c]}else{if(-1!=e){return orderPreference[e]}}}return 0}function runCloseLogic(a,f){var c=-1;var e=-1;var b=-1;for(var d=0;7>d;++d){if(3>a[orderPreference[d]]){if(3>f[orderPreference[d]]){e=d;break}else{if(-1==c){c=d}}}else{if(-1==b&&3<=f[orderPreference[d]]){b=d}}}if(-1!=e){return orderPreference[e]}else{if(-1!=c){return orderPreference[c]}else{if(-1!=b){return orderPreference[b]}}}return 0}function numHits(g,c,f){var e=g+c*2.5;var b=g-c*2.5;var d=(Math.random()*(e-b))*100;var a;if(1!=d){a=Math.round((Math.random()*(e-b))+b)}else{var h=Math.random()*2;if(0==h){a=0}else{if("B"==f){a=2}else{a=3}}}if(0>a){a=0}return a};
+var orderPreference = ["20", "19", "18", "17", "16", "15", "B"];
+function runPointLogic(a, f) {
+    var c = -1;
+    var e = -1;
+    var b = -1;
+    for (var d = 0; 7 > d; ++d) {
+        if (3 <= a[orderPreference[d]] && 3 > f[orderPreference[d]]) {
+            b = d;
+            break
+        } else {
+            if (3 > a[orderPreference[d]] && 3 > f[orderPreference[d]] && -1 == c) {
+                c = d
+            } else {
+                if (3 > a[orderPreference[d]] && 3 <= f[orderPreference[d]] && -1 == e) {
+                    e = d
+                }
+            }
+        }
+    }
+    if (-1 != b) {
+        return orderPreference[b]
+    } else {
+        if (-1 != c) {
+            return orderPreference[c]
+        } else {
+            if (-1 != e) {
+                return orderPreference[e]
+            }
+        }
+    }
+    return 0
+}
+function runCloseLogic(a, f) {
+    var c = -1;
+    var e = -1;
+    var b = -1;
+    for (var d = 0; 7 > d; ++d) {
+        if (3 > a[orderPreference[d]]) {
+            if (3 > f[orderPreference[d]]) {
+                e = d;
+                break
+            } else {
+                if (-1 == c) {
+                    c = d
+                }
+            }
+        } else {
+            if (-1 == b && 3 <= f[orderPreference[d]]) {
+                b = d
+            }
+        }
+    }
+    if (-1 != e) {
+        return orderPreference[e]
+    } else {
+        if (-1 != c) {
+            return orderPreference[c]
+        } else {
+            if (-1 != b) {
+                return orderPreference[b]
+            }
+        }
+    }
+    return 0
+}
+function numHits(g, c, f) {
+    var e = g + c * 2.5;
+    var b = g - c * 2.5;
+    var d = (Math.random() * (e - b)) * 100;
+    var a;
+    if (1 != d) {
+        a = Math.round((Math.random() * (e - b)) + b)
+    } else {
+        var h = Math.random() * 2;
+        if (0 == h) {
+            a = 0
+        } else {
+            if ("B" == f) {
+                a = 2
+            } else {
+                a = 3
+            }
+        }
+    }
+    if (0 > a) {
+        a = 0
+    }
+    return a
+}
+;
--- game/twoplayer.js
+++ game/twoplayer.js
@@ -1,1 +1,52 @@
-$(document).ready(function(){$("div.mark").addClass("userPositive");loadHits();$("div#undoToggle").click(function(){$("div#undoToggle").toggleClass("userNegative");$("div#undoToggle").toggleClass("userPositive");if("UNDO"==$("div#undoToggle").text()){$("div#undoToggle").text("DONE");if(null!=endGame){clearInterval(endGame);endGame=null;$("div.mark").removeAttr("style")}}else{$("div#undoToggle").text("UNDO");checkForEndGame()}$("div.mark").toggleClass("userNegative");$("div.mark").toggleClass("userPositive")});$("div.mark").click(function(){if("UNDO"==$("div#undoToggle").text()){if(null==endGame){addHit(this)}else{reset()}}else{removeHit(this)}})});function reset(){$("div.mark").text("");$("div#teamOnePoints").text("0");$("div#teamTwoPoints").text("0");$("div#undoToggle").text("UNDO");$("div.mark").addClass("userPositive");$("div.mark").removeClass("userNegative");$("div#undoToggle").addClass("userNegative");$("div#undoToggle").removeClass("userPositive");$("div.mark").removeAttr("style");for(hit in t1MarkHits){t1MarkHits[hit]=0;t2MarkHits[hit]=0}if(null!=endGame){clearInterval(endGame);endGame=null}};
+$(document).ready(function() {
+    $("div.mark").addClass("userPositive");
+    loadHits();
+    $("div#undoToggle").click(function() {
+        $("div#undoToggle").toggleClass("userNegative");
+        $("div#undoToggle").toggleClass("userPositive");
+        if ("UNDO" == $("div#undoToggle").text()) {
+            $("div#undoToggle").text("DONE");
+            if (null != endGame) {
+                clearInterval(endGame);
+                endGame = null;
+                $("div.mark").removeAttr("style")
+            }
+        } else {
+            $("div#undoToggle").text("UNDO");
+            checkForEndGame()
+        }
+        $("div.mark").toggleClass("userNegative");
+        $("div.mark").toggleClass("userPositive")
+    });
+    $("div.mark").click(function() {
+        if ("UNDO" == $("div#undoToggle").text()) {
+            if (null == endGame) {
+                addHit(this)
+            } else {
+                reset()
+            }
+        } else {
+            removeHit(this)
+        }
+    })
+});
+function reset() {
+    $("div.mark").text("");
+    $("div#teamOnePoints").text("0");
+    $("div#teamTwoPoints").text("0");
+    $("div#undoToggle").text("UNDO");
+    $("div.mark").addClass("userPositive");
+    $("div.mark").removeClass("userNegative");
+    $("div#undoToggle").addClass("userNegative");
+    $("div#undoToggle").removeClass("userPositive");
+    $("div.mark").removeAttr("style");
+    for (hit in t1MarkHits) {
+        t1MarkHits[hit] = 0;
+        t2MarkHits[hit] = 0
+    }
+    if (null != endGame) {
+        clearInterval(endGame);
+        endGame = null
+    }
+}
+;
